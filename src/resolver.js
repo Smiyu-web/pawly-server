33f2f12788e8d46b856eee9172ea782be6993dec
Miu <miyuuu0728@gmail.com>
2021-08-24 16:52:07 -0700

    added update

--- src/resolver.js
+++ src/resolver.js
@@ -3,7 +3,7 @@ import { GraphQLDate, GraphQLTime, GraphQLDateTime } from "graphql-iso-date";
 
 // Debug data in mongoDB
 (async () => {
-  console.log("Weight", await Weight.find());
+  // console.log("Weight", await Weight.find());
 })``;
 
 export const resolvers = {
@@ -23,5 +23,40 @@ export const resolvers = {
       await todayWeight.save();
       return todayWeight;
     },
+    updateWeight: async (_, { day, updateWeightNum }) => {
+      const data = await Weight.findOne({ day: day });
+      console.log(data);
+      if (!data) {
+        throw new Error("Editable data doesn't exist");
+      }
+      await data.updateOne({ weightNum: updateWeightNum });
+      return data;
+    },
   },
 };
+
+// export const UPDATE_PASSWORD = {
+//   type: MessageType,
+//   args: {
+//     username: { type: GraphQLString },
+//     oldPassword: { type: GraphQLString },
+//     newPassword: { type: GraphQLString },
+//   },
+//   async resolve(parent: any, args: any) {
+//     const { username, oldPassword, newPassword } = args;
+//     const user = await Users.findOne({ username: username });
+
+//     if (!user) {
+//       throw new Error("USERNAME DOESNT EXIST");
+//     }
+//     const userPassword = user?.password;
+
+//     if (oldPassword === userPassword) {
+//       await Users.update({ username: username }, { password: newPassword });
+
+//       return { successful: true, message: "PASSWORD UPDATED" };
+//     } else {
+//       throw new Error("PASSWORDS DO NOT MATCH!");
+//     }
+//   },
+// };
